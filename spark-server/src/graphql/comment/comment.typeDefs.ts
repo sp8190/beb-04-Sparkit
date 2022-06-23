051f5c16aad4c543ab86447dd3de9a4aa5d47481
HAEJEONG YU <haejeongHY@gmail.com>
2022-06-23 12:22:01 +0900

    Feat: 댓글 생성 기능 추가

---
+++ spark-server/src/graphql/comment/comment.typeDefs.ts
@@ -0,0 +1,12 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  type Comment {
+    post_id:Int,
+    user_id:Int,
+    comment:String
+  }
+  type Mutation {
+    createComment(post_id:Int, user_id:Int, comment:String):Int
+  }
+`;
