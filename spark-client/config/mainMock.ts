f3df37094ff06917ec459769b5689c58feb23330
lumpenop <swk5373@gmail.com>
2022-06-23 14:14:44 +0900

    Add: main mock 데이터

---
+++ spark-client/config/mainMock.ts
@@ -0,0 +1,9 @@
+export const ARR = Array.from({ length: 10 }, (v, i) => {
+  return {
+    id: i,
+    title: "제목",
+    postContent: "제곧내",
+    userId: "string",
+    createdAt: "date",
+  };
+});
