fe821f17f3aaa6f6c8bd37ed215a775e773ff1a9
HAEJEONG YU <haejeongHY@gmail.com>
2022-06-24 13:18:46 +0900

    Feat: 해쉬태그 목록 조회 시 태그한 포스트가 많은 순으로 조회하도록 업데이트

--- spark-server/src/graphql/hashtag/hashtag.resolvers.ts
+++ spark-server/src/graphql/hashtag/hashtag.resolvers.ts
@@ -1,5 +1,5 @@
-import hashtagModel from "../../models/hashtag.model";
 import {status} from "../../constants/code"
+import { sequelize } from '../../models/index';
 
 type hashtag = {
     id:number,
@@ -9,11 +9,12 @@ type hashtag = {
 export default {
     Query: {
         async getHashtags() {
-            let hashtags = await hashtagModel.findAll()
+            const getHashtagsOrderByCountQuery = `SELECT hashtags.id, hashtags.hashtag FROM posts_hashtags, hashtags where hashtags.id = posts_hashtags.hashtag_id group by posts_hashtags.hashtag_id order by count(posts_hashtags.hashtag_id) desc`
+            let hashtags = await sequelize.query(getHashtagsOrderByCountQuery, {})
             if(!hashtags) {
                 return status.SERVER_ERROR
             }
-            return hashtags
+            return hashtags[0]
         }
     }
 }
