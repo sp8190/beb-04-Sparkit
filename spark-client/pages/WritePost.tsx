6a5f3b204b76cb6e1d69cb2e12d625c69ea68b4d
Bingsu <56158143+sp8190@users.noreply.github.com>
2022-06-24 18:04:51 +0900

    WritePost.tsx css수정 및 mutation 구현

--- spark-client/pages/WritePost.tsx
+++ spark-client/pages/WritePost.tsx
@@ -1,59 +1,80 @@
-import { gql, useMutation } from '@apollo/client';
 import styled from 'styled-components';
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client'
+import { create } from "ipfs-http-client";
 
-const CREATE_POST = gql`
-  mutation CreatePost($postId: String!) {
-    createPost(id: $postId) {
-        id
-        title
-        post_content
-        image
-    }
+const CREATE_POST =gql`
+mutation CreatePost($title: String!, $post_content: String!, $user_id: Int, $hashtags: [String]) {
+    createPost(title: $title, post_content: $post_content, user_id: $user_id, hashtags: $hashtags)
   }
 `
-
+// 무시해 주셔도 됩니다. (잘 돌아가지만 수정할 예정)
+const client = create("https://ipfs.infura.io:5001/api/v0");
 
 export default function WritePost() {
     const [isImageUpload, setImageUploaded] = useState(false);
-    const [isdataURL, setdataURL] = useState("");
-    const [createPost, { data, loading, error }] = useMutation(CREATE_POST, {
-        variables: { postId: 1 } 
-    });
-    
+    const [isdataURL, setdataURL] = useState([]);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [hash, setHash] = useState("");
+    const [addNote, { loading, error }] = useMutation(CREATE_POST);
 
-     console.log(data)
+
+    const handleClick = () => {
+        console.log(title)
+        console.log(content)
+        console.log(hash)
+        console.log(isdataURL)
+        
+        // 아직 업로드 x
+        // addNote({ variables: { 
+        //     title: title,
+        //     post_content: content,
+        //     user_id: 1,
+        //     hashtags: [hash]
+        //  } });
+    };
+
+
 
     const uploadImage = async (f: any) => {
-        console.log("image upload", f.target.files[0])
+        //console.log("image upload", f.target.files[0])
+        const img = f.target.files[0];
         try {
-
-            const imgURL = URL.createObjectURL(f.target.files[0]);
+            const added = await client.add(img); //파일 업로드
+            const url = `https://ipfs.io/ipfs/${added.path}`;
+            console.log(url)
             setImageUploaded(true)
-            setdataURL(imgURL);
+            // 무시해 주셔도 됩니다. (잘 돌아가지만 수정할 예정)
+            setdataURL([...isdataURL, url]);
         }
         catch (err) {
             console.log("upload failed ", err)
         }
     }
-    
     return (
         <Wrapper>
             <FormWrap>
                 <TitleBox>
                     <label className='title'>Posting to</label>
-                    <input className='input' type="text" placeholder="제목" required></input>
+                    <input className='input' type="text" 
+                    onChange={({ target: { value } }) => setTitle(value)}
+                    placeholder="제목" required></input>
                 </TitleBox>
                 <ContentBox>
                     <label className='content'>컨텐츠</label>
-                    <textarea className='text' placeholder='내용을 입력하세요' required />
+                    <textarea className='text' 
+                    onChange={({ target: { value } }) => setContent(value)}
+                    placeholder='내용을 입력하세요' required />
                 </ContentBox>
                 <HashBox>
-                    <input className='hashtag' placeholder='#hash' required />
+                    <input className='hashtag' 
+                    onChange={({ target: { value } }) => setHash(value)}
+                    placeholder='#hash' required />
                 </HashBox>
                 <ImageBox>
                     <label className='image'>사진업로드</label>
-                    <input className='upload' onChange={uploadImage} type="file" accept="image/*" placeholder='이미지업로드하세요' required></input>
+                    <input className='upload' onChange={uploadImage} type="file" accept="image/*" required></input>
                     {
                         isImageUpload ?
                             loading ? "Loading image..." : "Upload complete!"
@@ -61,12 +82,20 @@ export default function WritePost() {
                     }
                 </ImageBox>
             </FormWrap>
-            <div style={{
-                height: '40%',
-                width: '40%'
-            }}>
-                <img aria-label="post image upload" src={isdataURL} />
-            </div>
+
+
+            {isdataURL.map((token) => {
+                return (
+                    <div style={{
+                        height: '40%',
+                        width: '40%'
+                    }}>           
+                        <img alt='imgURL' src={token}/>
+                     </div>
+                );
+            })}
+            
+            <button onClick={handleClick}>제출</button>
         </Wrapper>
 
 
