e6f606d0e57316094c3df03dfec7f13bc0e76a3a
HAEJEONG YU <haejeongHY@gmail.com>
2022-06-24 10:18:56 +0900

    Feat: 로그인 시 엑세스 토큰, 리프레시 토큰 발급

--- spark-server/src/graphql/user/user.typeDefs.ts
+++ spark-server/src/graphql/user/user.typeDefs.ts
@@ -12,8 +12,13 @@ export default gql`
     created_at:String,
     updated_at:String
   }
+  type Token {
+    access_token:String,
+    refresh_token:String
+
+  }
   type Mutation {
     createUser(email:String!, password:String!, nickname:String!, account:String!, balance:String!, private_key:String!):Boolean!
-    login(email:String!, password:String!):User
+    login(email:String!, password:String!):Token
   }
 `;
