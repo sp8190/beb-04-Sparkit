edd31193cb4b5c67429368baf8b692ee15b5f95b
lumpenop <swk5373@gmail.com>
2022-06-29 20:56:40 +0900

    Style: 안쓰는 코드 제거

--- spark-server/src/graphql/like/like.resolvers.ts
+++ spark-server/src/graphql/like/like.resolvers.ts
@@ -1,51 +1,58 @@
 import likeModel from "../../models/like.model";
 import userModel from "../../models/user.model";
-import {status} from "../../constants/code"
-import { sequelize } from '../../models/index';
-import { voteToOtherUser } from '../../token/tokenUtil'
-import { verifyAccessToken } from '../../utils/jwt';
+import { status } from "../../constants/code";
+import { sequelize } from "../../models/index";
+import { voteToOtherUser } from "../../token/tokenUtil";
+import { verifyAccessToken } from "../../utils/jwt";
 
 type inputLike = {
-    post_id:number,
-    user_id:number,
-    access_token:string
-}
+  post_id: number;
+  user_id: number;
+  access_token: string;
+};
 
 export default {
-    Mutation: {
-        async createLikes(_:any, args: inputLike) {
-            if(!verifyAccessToken(args.access_token)){
-                return status.TOKEN_EXPIRED
-            }
-            
-            let like = await likeModel.create({
-                post_id:args.post_id,
-                user_id:args.user_id
-            })
+  Mutation: {
+    async createLikes(_: any, args: inputLike) {
+      if (!verifyAccessToken(args.access_token)) {
+        return status.TOKEN_EXPIRED;
+      }
 
-            if(!like) {
-                return status.SERVER_ERROR
-            }
+      let like = await likeModel.create({
+        post_id: args.post_id,
+        user_id: args.user_id,
+      });
 
-            let senderInfo = await userModel.findOne({
-                where: {
-                    id:args.user_id
-                }
-            })
+      if (!like) {
+        return status.SERVER_ERROR;
+      }
 
-            const getReceiverQuery = `select users.* from posts, users where posts.id = :post_id and users.id = posts.user_id`
-            const getReceiverValue = {
-                post_id:args.post_id
-            }
-            const receiverInfoResult = await sequelize.query(getReceiverQuery, {replacements: getReceiverValue})
-            const receiverInfo = Object(receiverInfoResult[0][0])
-            
-            if(senderInfo && receiverInfo) {
-                voteToOtherUser(senderInfo.account, senderInfo.private_key, senderInfo.id, receiverInfo.account, receiverInfo.id)
+      let senderInfo = await userModel.findOne({
+        where: {
+          id: args.user_id,
+        },
+      });
 
-            }
+      const getReceiverQuery = `select users.* from posts, users where posts.id = :post_id and users.id = posts.user_id`;
+      const getReceiverValue = {
+        post_id: args.post_id,
+      };
+      const receiverInfoResult = await sequelize.query(getReceiverQuery, {
+        replacements: getReceiverValue,
+      });
+      const receiverInfo = Object(receiverInfoResult[0][0]);
 
-            return status.SUCCESS
-        }
-    }
-}
+      if (senderInfo && receiverInfo) {
+        voteToOtherUser(
+          senderInfo.account,
+          senderInfo.private_key,
+          senderInfo.id,
+          receiverInfo.account,
+          receiverInfo.id
+        );
+      }
+
+      return status.SUCCESS;
+    },
+  },
+};
