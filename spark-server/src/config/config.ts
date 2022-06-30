796a7468cad2a3f5be4d99c2abb48bb603dcb944
HAEJEONG YU <haejeongHY@gmail.com>
2022-06-30 15:56:31 +0900

    Refactor: eslint 사용하여 코드 수정

--- spark-server/src/config/config.ts
+++ spark-server/src/config/config.ts
@@ -1,23 +1,24 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+dotenv.config();
 
-// .env 파일 항목 데이터 타입 string 명시 
-declare var process : {
+// .env 파일 항목 데이터 타입 string 명시
+declare let process: {
   env: {
-    SPARK_IT_DB_USERNAME: string,
-    SPARK_IT_DB_PASSWORD: string,
-    SPARK_IT_DB_DATABASE: string,
-    SPARK_IT_DB_ENDPOINT: string,
-    SPARK_IT_DB_PORT: string
-  }
-}
+    SPARK_IT_DB_USERNAME: string;
+    SPARK_IT_DB_PASSWORD: string;
+    SPARK_IT_DB_DATABASE: string;
+    SPARK_IT_DB_ENDPOINT: string;
+    SPARK_IT_DB_PORT: string;
+  };
+};
 
 export const config = {
   development: {
-      username: process.env.SPARK_IT_DB_USERNAME,
-      password: process.env.SPARK_IT_DB_PASSWORD,
-      database: process.env.SPARK_IT_DB_DATABASE,
-      host: process.env.SPARK_IT_DB_ENDPOINT,
-      dialect: 'mysql',
-      port: process.env.SPARK_IT_DB_PORT
+    username: process.env.SPARK_IT_DB_USERNAME,
+    password: process.env.SPARK_IT_DB_PASSWORD,
+    database: process.env.SPARK_IT_DB_DATABASE,
+    host: process.env.SPARK_IT_DB_ENDPOINT,
+    dialect: "mysql",
+    port: process.env.SPARK_IT_DB_PORT,
   },
 };
